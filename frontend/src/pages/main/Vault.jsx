ba0a678c8f68e1be12ebf9e4b3648960c0cabee3
Jeisson-Acosta <juanacostamillos69@gmail.com>
2026-08-24 22:25:14 -0500

    feat: Se avanza en crear nuevas entradas en la modal, la informacion se guarda exitosamente, el siguiente paso antes de continuar es componetizar la modal y sus funcionalidades para tener todo mas organizado y despues se procede con mostrar las entradas con todos sus estilos

--- frontend/src/pages/main/Vault.jsx
+++ frontend/src/pages/main/Vault.jsx
@@ -3,6 +3,11 @@ import { useUserLogin } from '../../hooks/useUserLogin.js'
 import { useRequestDB } from '../../hooks/utils/useRequestDB.js'
 import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
+import { GlobeIcon, LockSimpleIcon, PlusIcon, UserCircleIcon } from '../../components/Icons.jsx'
+import { IconsEntries } from '../../components/IconsEntries.jsx'
+import { Input } from '../../components/Input.jsx'
+
+import { useInput } from '../../hooks/useInput.js'
 
 function CardHeader({ title, value, colorValue }) {
     return (
@@ -17,26 +22,68 @@ function CardHeader({ title, value, colorValue }) {
 
 export function Vault() {
 
+    const [showModalCreateEntry, setShowModalCreateEntry] = useState(false)
+    const [activeIconEntry, setActiveIconEntry] = useState('LockSimpleIcon')
+    const [selectedCategory, setSelectedCategory] = useState(null)
+
     const [infoVault, setInfoVault] = useState(null)
     const [filterActive, setFilterActive] = useState('TOD')
     const { userLogin } = useUserLogin()
     const { requestDB, isLoading } = useRequestDB()
 
-    useEffect(() => {
+    const nameServiceEntry = useInput('')
+    const userEntry = useInput('')
+    const passwordEntry = useInput('')
 
-        const infoVault = async () => {
-            const resultDB = await requestDB(`main/get-info-vault/${userLogin.usuid}`, 'GET')
-            if (!resultDB.ok) {
-                toast.error(resultDB.message)
-                return
-            }
-            resultDB.data[0].log_list = resultDB.data[0].log_list ? JSON.parse(resultDB.data[0].log_list) : resultDB.data[0].log_list
-            resultDB.data[0].category_list = resultDB.data[0].category_list ? JSON.parse(resultDB.data[0].category_list) : resultDB.data[0].category_list
-            resultDB.data[0].passwords_list = resultDB.data[0].passwords_list ? JSON.parse(resultDB.data[0].passwords_list) : resultDB.data[0].passwords_list
-            setInfoVault(resultDB.data[0])
+    const getInfoVault = async () => {
+        const resultDB = await requestDB(`main/get-info-vault/${userLogin.usuid}`, 'GET')
+        if (!resultDB.ok) {
+            toast.error(resultDB.message)
+            return
         }
-        infoVault()
+        resultDB.data[0].log_list = resultDB.data[0].log_list ? JSON.parse(resultDB.data[0].log_list) : resultDB.data[0].log_list
+        resultDB.data[0].category_list = resultDB.data[0].category_list ? JSON.parse(resultDB.data[0].category_list) : resultDB.data[0].category_list
+        resultDB.data[0].passwords_list = resultDB.data[0].passwords_list ? JSON.parse(resultDB.data[0].passwords_list) : resultDB.data[0].passwords_list
+        setInfoVault(resultDB.data[0])
+        setSelectedCategory(resultDB.data[0].category_list.find(cat => cat.catcod === 'PER').catid)
+    }
 
+
+    const handleClickSaveEntry = async () => {
+
+        if (!nameServiceEntry.value || !userEntry.value || !passwordEntry.value) {
+            toast.error('Todos los campos deben de estar diligenciados')
+            return
+        }
+
+        const infoToSave = {
+            connom: nameServiceEntry.value,
+            conusuario: userEntry.value,
+            conpwd: passwordEntry.value,
+            connom_icon: activeIconEntry,
+            usuid: userLogin.usuid,
+            catid: selectedCategory,
+        }
+
+        const resultDB = await requestDB('main/create-app', 'POST', infoToSave)
+        if (!resultDB.ok) {
+            toast.error(resultDB.message)
+            return
+        }
+
+        toast.success('¡Entrada creada correctamente!')
+        setShowModalCreateEntry(false)
+        setActiveIconEntry('LockSimpleIcon')
+        setSelectedCategory(null)
+
+        getInfoVault()
+
+    }
+
+    useEffect(() => {
+        const infoVault = async () => getInfoVault()
+
+        infoVault()
     }, [])
 
     if (isLoading || !infoVault) return
@@ -78,7 +125,110 @@ export function Vault() {
                     </div>
                 }
                 
-                {!infoVault.passwords_list && <h2 style={{color: 'var(--subtitlesColor)', textAlign: 'center'}}>Aún no tienes entradas. ¡Crea tu primera entrada ahora mismo!</h2>}
+                {!infoVault.passwords_list 
+                    && (
+                        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '20px'}}>
+                            <h2 style={{color: 'var(--subtitlesColor)', textAlign: 'center'}}>Aún no tienes entradas. ¡Crea tu primera entrada ahora mismo!</h2>                            
+                            <button onClick={() => setShowModalCreateEntry(true)}>
+                                <PlusIcon />
+                                Nueva Entrada
+                            </button>
+                        </div>
+                    )
+                }
+
+                {
+                    showModalCreateEntry && (
+                        <aside className='modal-create-entry'>
+                            <section className='container-fields-modal'>
+                                <header>
+                                    <h2 style={{color: 'var(--principalTitleColor)'}}>
+                                        Crear entrada
+                                    </h2>
+                                    <p>
+                                        Añade un nuevo servicio para tu lista de entradas
+                                    </p>
+                                </header>
+
+                                <div className='container-all-fields'>
+                                    <div style={{marginBottom: '12px'}}>
+                                        <h5 style={{fontFamily: 'fontSubtitle', fontSize: '13px', color: 'var(--subtitlesColor)', fontWeight: 'normal', marginBottom: '8px'}}>
+                                            Icono del servicio
+                                        </h5>
+                                        <div className='container-icons-service'>
+                                            {Object.keys(IconsEntries).map(icon => {
+
+                                                const Icon = IconsEntries[icon]
+
+                                                return (
+                                                    <div 
+                                                        key={icon}
+                                                        className={`icon-service ${icon === activeIconEntry ? 'active' : ''}`}
+                                                        onClick={() => setActiveIconEntry(icon)}
+                                                    >
+                                                        <Icon />
+                                                    </div>
+                                                )
+                                            })}
+                                        </div>
+                                    </div>
+                                    <label htmlFor="service-name">
+                                        Nombre del Servicio
+                                        <Input 
+                                            icon={<GlobeIcon />}  
+                                            id={'service-name'}
+                                            type={'text'}
+                                            placeholder={'EJ: Github, Netflix, Gmail...'}
+                                            {...nameServiceEntry}
+                                        />
+                                    </label>
+                                    <label htmlFor="category">
+                                        Categoría
+                                        <select id="category" onChange={(e) => setSelectedCategory(e.target.value)}>
+                                            {infoVault.category_list.map(category => (
+                                                <option value={category.catid}>
+                                                    {category.catnom}
+                                                </option>
+                                            ))}
+                                        </select>
+                                    </label>
+                                    <label htmlFor="username">                                        
+                                        Usuario o Correo
+                                        <Input 
+                                            icon={<UserCircleIcon />}  
+                                            id={'username'}
+                                            type={'text'}
+                                            placeholder={'nombre@dominio.com'}
+                                            {...userEntry}
+                                        />                                        
+                                    </label>
+                                    <label htmlFor="password">                                        
+                                        Contraseña
+                                        <Input 
+                                            icon={<LockSimpleIcon />}  
+                                            id={'password'}
+                                            type={'password'}
+                                            placeholder={'●●●●●●●●●●'}
+                                            {...passwordEntry}
+                                        />                                        
+                                    </label>
+                                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '16px'}}>
+                                        <button 
+                                            style={{width: '100%', backgroundColor: 'transparent', color: 'var(--mainColor)', border: '1px solid #252b3a'}}
+                                            onClick={() => setShowModalCreateEntry(false)}
+                                        >
+                                            Cancelar
+                                        </button>
+                                        <button style={{width: '100%'}} onClick={handleClickSaveEntry}>
+                                            Guardar Entrada
+                                        </button>
+                                    </div>
+                                </div>
+
+                            </section>
+                        </aside>
+                    )
+                }
             </div>
 
 
